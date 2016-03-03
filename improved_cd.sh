44101ae3f49ec5688783ef4eef22426ca7e92e59
garfield <b4rtem@gmail.com>
2016-03-03 23:56:42 +0100

    Added history to cd

---
+++ improved_cd.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+declare -a __SAVED_DIRS__
+declare -a __CD_HISTORY__
+
+function saved()
+{
+  if [[ $# = 0 ]]; then
+    if [[ ${#__SAVED_DIRS__[@]} = 0 ]]; then
+      echo -n "No directories saved"
+    fi
+    for ((i = 1 ; i <= ${#__SAVED_DIRS__[@]} ; ++i)) ; do
+      echo -n "#$i: ${__SAVED_DIRS__[$i]} "
+    done
+    echo
+  else
+    local dir
+    for dir in $@; do
+      if [[ $dir =~ ^--[0-9]+$ ]]; then
+        local num=${dir:2}
+        if [[ $num -gt ${#__CD_HISTORY__[@]} ]]; then
+          echo "No entry $num in history"
+          return
+        else
+          dir=${__CD_HISTORY__[$num]}
+        fi
+      else
+        dir=`realpath $dir`
+      fi
+      if [[ -d $dir ]]; then
+        for i in `seq 1 ${#__SAVED_DIRS__[@]}`; do
+          if [[ ${__SAVED_DIRS__[$i]} = $dir ]]; then
+            unset __SAVED_DIRS__[$i]
+          fi
+        done
+        __SAVED_DIRS__=([1]=$dir "${__SAVED_DIRS__[@]}")
+      else
+        echo "$dir: No such directory" >&2
+      fi
+    done
+  fi
+}
+
+function dropd()
+{
+  function repair_indexes()
+  {
+    __SAVED_DIRS__=([0]="" "${__SAVED_DIRS__[@]}")
+    unset __SAVED_DIRS__[0]
+  }
+
+  if [[ $# = 0 ]]; then
+    unset __SAVED_DIRS__[1]
+    repair_indexes
+  else
+    for arg in $@; do
+      if [[ $arg =~ ^[0-9]+$ ]]; then
+        unset __SAVED_DIRS__[$arg]
+      else
+        echo "$arg: invalid argument"
+      fi
+    done
+    repair_indexes
+  fi
+
+  unset -f repair_indexes
+}
+
+function cd()
+{
+  function add_to_hist()
+  {
+    local HIST_SIZE=${#__CD_HISTORY__[@]}
+    if [[ ${__CD_HISTORY__[HIST_SIZE]} != $1 ]]; then
+      __CD_HISTORY__[$(( HIST_SIZE + 1 ))]=$1
+    fi
+  }
+
+  if [[ $# -gt 0 ]] && [[ $1 =~ ^-[0-9]+$ ]]; then
+    local num=${1:1}
+    if [[ $num > ${#__SAVED_DIRS__[@]} ]]; then
+      echo "No entry $num in saved directories"
+    else
+      echo "${__SAVED_DIRS__[$num]}"
+      command cd ${__SAVED_DIRS__[$num]}
+      add_to_hist ${__SAVED_DIRS__[$num]}
+    fi
+  elif [[ $1 =~ ^-h ]]; then
+    local LIMIT
+    if [[ ${1:2} =~ ^[0-9]+$ ]]; then
+      LIMIT=${1:2}
+    else
+      LIMIT=10
+    fi
+    local HIST_SIZE=${#__CD_HISTORY__[@]}
+    local MIN=$(( $HIST_SIZE - $LIMIT ))
+    MIN=$(( $MIN < 1 ? 1 : $MIN ))
+    for (( i = $MIN ; i <= $HIST_SIZE ; ++i )); do
+      echo "$i: ${__CD_HISTORY__[$i]}"
+    done
+  elif [[ $1 =~ ^--[0-9]+$ ]]; then
+    local num=${1:2}
+    if [[ $num -gt ${#__CD_HISTORY__[@]} ]]; then
+      echo "No entry $num in history"
+    else
+      local dir=${__CD_HISTORY__[$num]}
+      echo $dir
+      command cd $dir
+      add_to_hist $dir
+    fi
+  else
+    for arg in $@; do
+      if [[ -d $arg ]]; then
+        add_to_hist `realpath $arg`
+        break
+      fi
+    done
+    if [[ $# = 0 ]]; then
+      add_to_hist $HOME
+    elif [[ $1 = '-' ]]; then
+      add_to_hist $OLDPWD
+    fi
+    command cd $@
+  fi
+
+  unset -f add_to_hist
+}
+
